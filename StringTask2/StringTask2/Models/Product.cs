aff3396a272abde6b6344088dc67819748e61cab
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-14 23:08:43 +0400

    added

---
+++ StringTask2/StringTask2/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask2.Models
+{
+    public class Product
+    {
+        public string Id;
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int Income;
+        public int Count;
+
+        public Product(string brandName, string model, int price, int cost, int count)
+        {
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Count = count;
+
+        }
+        public static string IdMaker(string name, string model)
+        {
+            string id = name.Substring(0, 2) + model.Substring(0, 2);
+            return id;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Brand {BrandName}, Model: {Model} Price{Price}, Count: {Count} Income: {Income}");
+        }
+
+    }
+}
---
+++ StringTask2/StringTask2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringTask2;
+using StringTask2.Models;
+
+public class Program : Product
+{
+    public Program(string brandName, string model, int price, int cost, int count) : base(brandName, model, price, cost, count)
+    {
+    }
+
+    static void Main(string[] args)
+    {
+        Product product = new Product("apple", "iphone", 999, 600, 17);
+        Console.WriteLine(string.Concat("ID: ", IdMaker(product.BrandName, product.Model)));
+
+    }
+
+
+
+
+
+}
